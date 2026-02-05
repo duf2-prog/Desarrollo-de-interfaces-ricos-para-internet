ec4f7a8cc06c9d680771597d416a77a07b53d981
Daniel <duf2@gcloud.ua.es>
2026-02-05 11:35:25 +0100

    Revertir cambios

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   return {
     base: env.VITE_APP_BASE_URL || '/',
     build: {
-      outDir: 'dist',
+      outDir: 'docs',
     },
     plugins: [react()],
   }
